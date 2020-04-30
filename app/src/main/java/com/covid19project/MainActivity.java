868e0687352e4ff2582d2043098f841c63ac01eb
Dharaneeshwar <daranip@gmail.com>
2020-04-30 15:35:53 +0530

    Health status developed

--- app/src/main/java/com/covid19project/MainActivity.java
+++ app/src/main/java/com/covid19project/MainActivity.java
@@ -95,8 +95,9 @@ public class MainActivity extends AppCompatActivity {
 //                        Intent intent7 = new Intent(MainActivity.this, CounsellingActivity.class);
 //                        startActivity(intent7);
 //                        break;
-                        String url = "http://maps.google.co.uk/maps?q=Pharmacy&hl=en"
-                        Intent intent7 = new Intent(Intent.ACTION_VIEW,Uri.parse(url));      intent.setClassName("com.google.android.apps.maps","com.google.android.maps.MapsActivity");
+                        String url = "http://maps.google.co.uk/maps?q=Pharmacy&hl=en";
+                        Intent intent7 = new Intent(Intent.ACTION_VIEW,Uri.parse(url));
+                        intent7.setClassName("com.google.android.apps.maps","com.google.android.maps.MapsActivity");
                         startActivity(intent7);
                     case 8:
                         Intent intent8 = new Intent(MainActivity.this, EPassActivity.class);
